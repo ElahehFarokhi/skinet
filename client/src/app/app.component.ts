54c1737fcc6be3caa75d527878fa6957ca4f26f0
Elaheh Farokhi <elaheh.farokhi@gmail.com>
2024-11-13 14:28:45 +0330

    Add product item component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,35 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { HeaderComponent } from './layout/header/header.component';
 import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-baseUrl="https://localhost:5001/api/";
+export class AppComponent {
   title = 'skinet';
-constructor(private http: HttpClient){
-
-}
-products: Product[]=[];
-
-ngOnInit(): void{
-  this.http.get<Pagination<Product>>(this.baseUrl + 'products')
-  .subscribe({
-    next: response=>{
-      this.products = response.data
-    },
-    error: error => console.log('errod ===>',error),
-    complete: () => console.log('complete')
-  })
-}
-
-
 }
